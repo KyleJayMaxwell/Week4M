6ec3090756e3c020b84c7806874eefe3b6df352a
Kyle Maxwell <kylejaymaxwell@gmail.com>
2015-10-05 20:03:33 -0600

    1-6 Homework

--- jQuerying.js
+++ jQuerying.js
@@ -1,1 +1,50 @@
+$(document).ready(function(){
+	// Question 1
+	$("#hover").mouseenter(function(){
+		$(this).css("background-color", "red");
+	});
+	$("#hover").mouseleave(function(){
+		$(this).css("background-color", "#39B7CD")
+	});
+	$("#hover").click(function(){
+		$(this).toggleClass("green");
+	});
 
+	// Question 2
+
+	$(".hideMe").click(function(){
+		$(this).remove();
+	});
+
+	// Question 3
+
+	$(".changeColor").click(function(){
+		$("ul li:nth-child(3)").css("color", "#8A2BE2");
+	});
+
+	// Question 4
+
+	$("#removeLi").click(function(){
+		$("ul li:nth-child(2)").remove();
+	});
+
+	// Question 5
+
+	$("ul li:nth-child(4)").click(function(){
+		$(this).append("<li>Fuck</li>")
+	});
+
+	// Question 6
+
+	$("ul li:last-child").click(function(){
+		$(this).css("font-size", "40px");
+		$("ul li:not(:last)").remove();
+	});
+
+
+
+
+
+
+
+});
